a4b999471db261ddc955e9b7fded06070728fec5
myhkas <mmyhkas@gmail.com>
2020-08-16 23:24:34 -0700

    Updates

---
+++ precache-manifest.e2e7f8b61a1a6aae04859eed42a82820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ce264185239662690f37a4cbda1095",
+    "url": "/User_Directory/index.html"
+  },
+  {
+    "revision": "5404e630a68d4507c176",
+    "url": "/User_Directory/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b28d945764db004c4384",
+    "url": "/User_Directory/static/js/2.d36225b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User_Directory/static/js/2.d36225b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5404e630a68d4507c176",
+    "url": "/User_Directory/static/js/main.124b4d74.chunk.js"
+  },
+  {
+    "revision": "2100e1b4829147c4f64f",
+    "url": "/User_Directory/static/js/runtime-main.49307f09.js"
+  }
+]);
